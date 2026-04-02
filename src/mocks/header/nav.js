721e968cb7a02309e96e3739a01ae0f4ff91c4cd
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-04-02 20:22:43 +0300

    chore(mocks): create mock data for header

---
+++ src/mocks/header/nav.js
@@ -0,0 +1,130 @@
+import FunSun from "../../components/icons/FunSun";
+
+export const NavMenuItems = [
+  {
+    id: 0,
+    hasIcon: false,
+    icon: null,
+    openMenu: false,
+    name: "Wibes",
+    link: "/wibes"
+  },
+  {
+    id: 1,
+    hasIcon: false,
+    icon: null,
+    openMenu: false,
+    name: "Отели",
+    link: "/hotels"
+  },
+  {
+    id: 2,
+    hasIcon: false,
+    icon: null,
+    openMenu: false,
+    name: "Авиабилеты",
+    link: "/avia-tickets"
+  },
+  {
+    id: 3,
+    hasIcon: true,
+    icon: FunSun,
+    openMenu: false,
+    name: "Туры с",
+    link: "/wibes"
+  },
+  {
+    id: 4,
+    hasIcon: false,
+    icon: null,
+    openMenu: false,
+    name: "Ресейл",
+    link: "/resale"
+  },
+  {
+    id: 5,
+    hasIcon: false,
+    icon: null,
+    openMenu: false,
+    name: "WB Клуб",
+    link: "/wb-club"
+  },
+  {
+    id: 6,
+    hasIcon: false,
+    icon: null,
+    openMenu: false,
+    name: "Бренды",
+    link: "/brands"
+  },
+  {
+    id: 7,
+    hasIcon: false,
+    icon: null,
+    openMenu: false,
+    name: "Новостройки",
+    link: "/buildings"
+  },
+  {
+    id: 8,
+    hasIcon: false,
+    icon: null,
+    openMenu: true,
+    name: "Для бизнеса",
+    link: "/buisness"
+  },
+  {
+    id: 2,
+    hasIcon: false,
+    icon: null,
+    openMenu: false,
+    name: "Работа в WB",
+    link: "/career"
+  },
+];
+
+export const MoreMenuItems = [
+  {
+    id: 0,
+    name: "Ресейл",
+    link: "/resale"
+  },
+  {
+    id: 1,
+    name: "Бренды",
+    link: "/brands"
+  },
+  {
+    id: 2,
+    name: "Новостройки",
+    link: "/buildings"
+  }
+];
+
+export const BuisnessMenuItems = [
+  {
+    id: 0,
+    name: "Покупайте как бизнес",
+    link: "/business",
+  },
+  {
+    id: 1,
+    name: "Стать продавцом",
+    link: "/become-seller",
+  },
+  {
+    id: 2,
+    name: "Открыть пункт выдачи",
+    link: "/pickup-points/open",
+  },
+  {
+    id: 3,
+    name: "Товары для бизнеса",
+    link: "/business-products",
+  },
+  {
+    id: 5,
+    name: "Оптом из Китая",
+    link: "/wholesale/china",
+  }
+];
