0747e554022f28caa70dc512ab4ae13834e5bc31
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-06-01 11:09:07 +0300

    update(:routes): update pages path

---
+++ src/routes/paths.js
@@ -0,0 +1,15 @@
+export const ROUTES = {
+  HOME: "/",
+  AUTH: {
+    ROOT: "/auth",
+    LOGIN: "/auth/login",
+    REGISTER: "/auth/register",
+  },
+};
+
+export const ROUTE_HANDLE = {
+  HOME: { title: "Главная" },
+  LOGIN: { title: "Вход" },
+  REGISTER: { title: "Регистрация" },
+  NOT_FOUND: { title: "Страница не найдена" },
+};
