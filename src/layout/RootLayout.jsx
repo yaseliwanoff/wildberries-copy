49fbd0f4f6b7f0339e9a360ce9464d8bdef4cc54
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-06-01 11:06:30 +0300

    update(layout): update root layout and create bases styles

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,16 +1,30 @@
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "../components/layout/Header/Header";
-import { Outlet } from "react-router-dom";
+import PageSuspense from "../components/routing/PageSuspense";
+import ScrollToTop from "../components/routing/ScrollToTop";
+import { useRouteTitle } from "../hooks/useRouteTitle";
+import styles from "./RootLayout.module.scss";
 
-const RootLayout = () => {
+function RootLayout() {
+  useRouteTitle();
+
   return (
-    <>
+    <div className={styles.root}>
+      <ScrollRestoration />
+      <ScrollToTop />
       <Header />
-      <main>
-        <Outlet />
+      <main className={styles.main}>
+        <PageSuspense>
+          <Outlet />
+        </PageSuspense>
       </main>
-      <footer>© 2026 My App</footer>
-    </>
-  )
+      <footer className={styles.footer}>
+        <div className="container">
+          <p className={styles.footerText}>© 2026 Wildberries</p>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default RootLayout;
