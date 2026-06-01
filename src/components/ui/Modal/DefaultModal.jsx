e3e75bb44e4508a23d88d7b6c4672716c87b8c92
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-06-01 11:01:02 +0300

    update(component): update default modal wrapper component

--- src/components/ui/Modal/DefaultModal.jsx
+++ src/components/ui/Modal/DefaultModal.jsx
@@ -1,24 +1,25 @@
-import "./DefaultModal.scss"
+import "./DefaultModal.scss";
 import { X } from "lucide-react";
+import { useScrollLock } from "../../../hooks/useScrollLock";
 
 const DefaultModal = ({ isVisible, children, onClose }) => {
+  useScrollLock(isVisible);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
-    <div>
-      { isVisible && (
-        <>
-          <div className="modal_background_full" onClick={onClose}></div>
-          <div className="modal_wrapper">
-            <button className="modal_close_btn" onClick={onClose}>
-              <X color="#9d9da5" />
-            </button>
-            <div className="modal_inner_wrapper">
-              { children }
-            </div>
-          </div>
-        </>
-      ) }
-    </div>
-  )
-}
+    <>
+      <div className="modal_background_full" onClick={onClose} />
+      <div className="modal_wrapper">
+        <button type="button" className="modal_close_btn" onClick={onClose}>
+          <X color="#9d9da5" />
+        </button>
+        <div className="modal_inner_wrapper">{children}</div>
+      </div>
+    </>
+  );
+};
 
 export default DefaultModal;
