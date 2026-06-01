7b84bd539d5d591bb91d74d57270b453ead5baeb
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-06-01 11:01:37 +0300

    update(search): update search modal component, fix background bug and new block scroll page

--- src/components/ui/Modal/SearchModal.jsx
+++ src/components/ui/Modal/SearchModal.jsx
@@ -1,46 +1,47 @@
 import { Link } from "react-router-dom";
 import { Search } from "lucide-react";
 import SearchModalItems from "../../../mocks/header/search-modal";
-import "./SearchModal.scss"
+import { useScrollLock } from "../../../hooks/useScrollLock";
+import "./SearchModal.scss";
 
 const SearchModal = ({ isVisible, onClose, headerHeight, searchingValue }) => {
+  useScrollLock(isVisible);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
-    <div>
-      {isVisible && (
-        <>
-          <div 
-            className="modal_background_black" 
-            style={{ top: headerHeight }} 
-            onClick={onClose}
-          />
-          <div className="modal_wrapper_search rounded-xl">
-            <p className="modal_title">Часто ищут</p>
-            <ul className="modal_inner_wrapper">
-              {searchingValue.length >= 1 ? (
-                <div className="modal_inner_item">
-                  <Link className="modal_inner_link">
-                    <Search className="modal_inner_icon" />
-                    <span>{searchingValue}</span>
-                  </Link>
-                </div>
-              ) : (
-                <>
-                  {SearchModalItems.map((item) => (
-                    <li key={item.id} className="modal_inner_item">
-                      <Link className="modal_inner_link">
-                        <Search className="modal_inner_icon" />
-                        <span>{item.title}</span>
-                      </Link>
-                    </li>
-                  ))}
-                </>
-              )}
-            </ul>
-          </div>
-        </>
-      )}
-    </div>
-  )
-}
+    <>
+      <div
+        className="modal_background_black"
+        style={{ top: headerHeight }}
+        onClick={onClose}
+      />
+      <div className="modal_wrapper_search rounded-xl">
+        <p className="modal_title">Часто ищут</p>
+        <ul className="modal_inner_wrapper">
+          {searchingValue.length >= 1 ? (
+            <li className="modal_inner_item">
+              <Link className="modal_inner_link">
+                <Search className="modal_inner_icon" />
+                <span>{searchingValue}</span>
+              </Link>
+            </li>
+          ) : (
+            SearchModalItems.map((item) => (
+              <li key={item.id} className="modal_inner_item">
+                <Link className="modal_inner_link">
+                  <Search className="modal_inner_icon" />
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default SearchModal;
