8d1f5559767c705d8a6502c4d41c75e2269430a9
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-06-12 15:36:27 +0300

    update(product card): new card, new styles card and edit elements inner position

--- src/components/products/MainProductCard/MainProductCard.jsx
+++ src/components/products/MainProductCard/MainProductCard.jsx
@@ -1,5 +1,7 @@
 import styles from "./MainProductCard.module.scss";
 import ProductPrice from "./ProductPrice";
+import ProductReviews from "./ProductReviews";
+import { Link } from "react-router-dom";
 
 const resolveImageSrc = (image) => {
   if (Array.isArray(image)) {
@@ -8,17 +10,27 @@ const resolveImageSrc = (image) => {
   return image ?? "";
 };
 
-const MainProductCard = ({ title, image, price, brand }) => {
+const MainProductCard = ({ title, image, rating, price, brand }) => {
   const imageSrc = resolveImageSrc(image);
 
   return (
     <div className={styles.product_card_wrapper}>
-      <img src={imageSrc} alt={title} className={styles.product_card_image} />
-      <div className={styles.product_card_info}>
-        {brand && <p className={styles.product_card_brand}>{brand}</p>}
-        <h3 className={styles.product_card_title}>{title}</h3>
-        {price && <ProductPrice price={price} />}
-      </div>
+      <Link to={"/"} className={styles.product_card_link_wrapper}>
+        <img src={imageSrc} alt={title} className={styles.product_card_image} />
+        <div className={styles.product_card_info}>
+          {price && <ProductPrice price={price} />}
+          <h2 className={styles.product_card_info_text}>
+            {brand && (
+              <span className={styles.product_card_brand}>{brand}</span>
+            )}
+            <span className={styles.product_card_title}>
+              / {`${title.substr(0, 10)}...`}
+            </span>
+          </h2>
+          {rating && <ProductReviews rating={rating} />}
+        </div>
+        <button className={styles.product_card_order}>Заказать</button>
+      </Link>
     </div>
   );
 };
